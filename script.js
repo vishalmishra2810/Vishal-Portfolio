d0eec444d807dd9166fbee46b613a70a35fa566f
Vishal Mishra <mishravishal2810@gmail.com>
2025-01-01 17:45:43 +0530

    changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer", "MERN Developer"],
+    strings: ["Web Developer", "Frontend Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Developer", "Software Engineer"],
+    strings: ["Web Developer", "Frontend Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
